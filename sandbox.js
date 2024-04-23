0cdacc82d413b96382365155eff6614f4d501fec
Mohammad Ashif <Mohammad Ashif>
2024-04-23 13:36:05 +0600

    ts file run by the tsc

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,7 @@
-// var character = 'mario';
-// console.log(character);
-// var inputs = document.querySelectorAll('input');
-// inputs.forEach(function (input) {
-//     console.log(input);
-// });
+var person = "aaashif";
+console.log(person);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+const person = "aaashif";
+
+console.log(person);
+
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+
+inputs.forEach(input => {
+    console.log(input);
+})
